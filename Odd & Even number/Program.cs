291ec0ee9a6525fbdd7b86be7c8de3b8bbc8f32d
Jahidulhasan6726 <126105121+Jahidulhasan6726@users.noreply.github.com>
2023-09-19 12:48:59 +0600

    Add files via upload

---
+++ Odd & Even number/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01_EviTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// for even number
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Run(i);
+                }
+            }
+
+            //for odd number
+            for (int i = 1; i <= 20; i++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        Run1(i);
+                    }
+                }
+
+            Console.ReadKey();
+        }
+        static void Run(int n)
+        {
+            Task.Run(() =>
+            {
+                return OddNumber(n);
+            }).ContinueWith(t =>
+            {
+                Console.WriteLine($"{n}!={t.Result}");
+            });
+        }
+        static async Task<long> OddNumber(int n)
+        {
+            int fact = 1;
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    fact = fact * i;
+                }
+            });
+            return fact;
+        }
+
+        static void Run1(int n)
+        {
+            Task.Run(() =>
+            {
+                return EvenNumber(n);
+            }).ContinueWith(t =>
+            {
+                Console.WriteLine($"{n}!={t.Result}");
+            });
+        }
+        static async Task<long> EvenNumber(int n)
+        {
+            int fact = 1;
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    fact = fact * i;
+                }
+            });
+            return fact;
+        }
+    }
+}
